1a21af1e79f40acb13eecf531e5b516a91684c44
Khanh Nguyen <khanhtnguyen@mavs.uta.edu>
2018-02-03 03:11:04 -0600

    added redux persist

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import logger from "redux-logger";
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import DefaultNavigator from './src/navigations/DefaultNavigator';
-import reducers from './src/reducers';
+import rootReducer from './src/reducers';
 
-const store = createStore(reducers, applyMiddleware(logger));
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(logger))
+const persistor = persistStore(store)
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <DefaultNavigator />
+        <PersistGate loading={null} persistor={persistor}>
+          <DefaultNavigator />
+        </PersistGate>
       </Provider>
     );
   }
